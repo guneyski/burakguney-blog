1f1e149fc2511679ac4feb390511025dd72d0041
Burak Guney <burakguney.net@outlook.com>
2020-03-04 13:48:08 +0300

    error

--- src/blog-posts.js
+++ src/blog-posts.js
@@ -1,15 +1,6 @@
 export const getPosts = () => {
   return [
     {
-      title: "Hakkımda",
-      slug: "hakkimda",
-      img: "burakguney1.jpg",
-      entrance:
-        "Edirne'nin Uzunköprü ilçesinde 23 Ağustos 1995'te doğdum. Bilgisayarla tanışmam 2000-2001 senelerine denk geliyor...",
-      details: require("./posts/hakkimda.md").default,
-      date: "3 Mart 2020"
-    },
-    {
       title: "İşletim Sistemleri-1",
       slug: "isletimSistemleri",
       img: "isletimSistemleri.png",
@@ -17,6 +8,15 @@ export const getPosts = () => {
         "Bu yazıda işlenen konular: işletim sistemi ne iş yapar, bilgisayar sistemi ve organizasyonu, bilgisayar sistemi mimarisi, işletim sistemi yapısı, işletim sistemi işlemleri.",
       details: require("./posts/isletimSistemleri.md").default,
       date: "4 Mart 2020"
+    },
+    {
+      title: "Hakkımda",
+      slug: "hakkimda",
+      img: "burakguney1.jpg",
+      entrance:
+        "Edirne'nin Uzunköprü ilçesinde 23 Ağustos 1995'te doğdum. Bilgisayarla tanışmam 2000-2001 senelerine denk geliyor...",
+      details: require("./posts/hakkimda.md").default,
+      date: "3 Mart 2020"
     },
     {
       title: "Burak Güney",
